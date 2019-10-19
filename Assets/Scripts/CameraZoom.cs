e559e384ea44b001b5390ab97966cc29ef5cf29f
Ryan-J-Kennedy <56158637+Ryan-J-Kennedy@users.noreply.github.com>
2019-10-19 16:10:24 +0200

    Camera
    Finished up the camera movement,
    Camera will move around with keys.
    Camera will zoom with scroll wheel.
    When camera is close to the ground then the camera rotates to look flatter.

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class CameraZoom : MonoBehaviour
 {
     public float zoomSpeed;
+    public float rotationSpeed;
+    
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        Vector3 position = new Vector3(transform.position.x, 2.1f , transform.position.z);
+
+        if (transform.position.y > 2f)
+        {
+            transform.position += transform.forward * Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        }
+
+        if(transform.position.y < 9f && transform.position.y > 2.2f)
+        {
+            transform.Rotate(Input.GetAxis("Mouse ScrollWheel") * rotationSpeed * -1f, 0f, 0f);
+        }
+
+        if (transform.position.y < 2f)
+        {
+            transform.position = position;
+        }
+        
     }
 }
